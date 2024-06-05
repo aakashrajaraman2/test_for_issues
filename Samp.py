2f165f6e52ededbb4afd27274a01daebb5f58b69
VishnukumarSR17 <156899748+VishnukumarSR17@users.noreply.github.com>
2024-06-05 11:15:41 +0530

    Add files via upload

---
+++ Samp.py
@@ -0,0 +1,35 @@
+import requests
+import json
+def create_issues(ACCESS_TOKEN,OWNER,REPO,title,body,labels):
+    headers = {
+    'Authorization': f'token {ACCESS_TOKEN}',
+    'Accept': 'application/vnd.github.v3+json'
+    }
+    url = f'https://api.github.com/repos/{OWNER}/{REPO}/issues'
+
+    # Define the issue data
+    issue_data = {
+        'title':title,
+        'body':body,
+        'labels':labels
+    }
+
+    # Send the POST request to create the new issue
+    response = requests.post(url, headers=headers, data=json.dumps(issue_data))
+
+    # Check the response status code
+    if response.status_code == 201:
+        print('New issue created successfully!')
+        print(response.json())
+    else:
+        print(f'Error creating issue: {response.status_code}')
+        print(response.text)
+
+# Replace these with your GitHub access token and repository information
+ACCESS_TOKEN = ''
+OWNER = ''
+REPO = ''
+body='The new issue raised is confirmed'
+title='Title-1'
+labels=['bug', 'high-priority']
+create_issues(ACCESS_TOKEN,OWNER,REPO,title,body,labels)
